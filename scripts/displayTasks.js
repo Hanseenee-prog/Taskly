cd592b73a9a3a2e7c5b3aefeb392e0897d5faee5
Hanson Emmanuel <hansonemma398@gmail.com>
2025-09-30 22:12:28 +0100

    Auto-sync: file changes

--- scripts/displayTasks.js
+++ scripts/displayTasks.js
@@ -1,21 +1,26 @@
 const tasks = [
     {
+        listId: 'task-1',
         name: 'Eat food',
         date: 'Today, 5:00pm'
     },
     {
+        listId: 'task-2',
         name: 'Wash plates',
         date: 'Tomorrow, 2:00pm'
     },
     {
+        listId: 'task-3',
         name: 'Read books',
         date: 'Next week, 12:00pm'
     },
     {
+        listId: 'task-4',
         name: 'Wash clothes',
         date: 'Next year, 12:00pm'
     },
     {
+        listId: 'task-5',
         name: 'Cook soup',
         date: 'Wednesday, 12:00pm'
     }
