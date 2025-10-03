f58ad4429d224d70840b844bd5108b9aa152f458
Hanson Emmanuel <hansonemma398@gmail.com>
2025-10-03 10:11:13 +0100

    Auto-sync: file changes

--- scripts/display/displayTasks.js
+++ scripts/display/displayTasks.js
@@ -36,14 +36,14 @@ export function displayTasks() {
                 </button>
                 
                 <button class="list-btn js-edit-btn">
-                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
                         <path d="M17 3a2.85 2.85 0 0 1 4 4L7.5 20.5 2 22l1.5-5.5L17 3z"></path>
                     </svg>
                 </button>
                 
                 <button class="list-btn js-done-with-task-btn">
-                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="roundppp
-                        <pp points="20 6 9 17 4 p"></polyline>
+                    <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M20 6L9 17L4 12" stroke="#000000" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
                     </svg>
                 </button>
             </div>
