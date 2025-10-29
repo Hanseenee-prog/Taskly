bb0a02f3be9de07dd384920788e3be6f09d2735a
Hanseenee-prog <hansonemma398@gmail.com>
2025-10-29 11:07:44 +0100

    update tasks.js

--- scripts/tasks.js
+++ scripts/tasks.js
@@ -10,36 +10,16 @@ export function loadFromStorage() {
   if (!tasks) {
     tasks = [{
         listId: 'task-1',
-        name: 'Eat food',
+        name: 'Add a task to continue',
         time: '05:00',          
-        date: '2025-12-13'      
-    },{
-        listId: 'task-2',
-        name: 'Wash plates',
-        time: '14:00',          
-        date: '2025-10-02'      
-    },{
-        listId: 'task-3',
-        name: 'Read books',
-        time: '23:00',          
-        date: '2025-10-08'      
-    },{
-        listId: 'task-4',
-        name: 'Wash clothes',
-        time: '19:00',          
-        date: '2026-10-01'      
-    },{
-        listId: 'task-5',
-        name: 'Cook soup',
-        time: '12:00',          
-        date: '2025-10-08'      
+        date: '2025-10-29'      
     }];
   }
   
   if (!completedTasks) {
     completedTasks = [{
         listId: 'task-5',
-        name: 'Cook soup',
+        name: 'You\'ve done this task',
         time: '12:00',          
         date: '2025-10-08'      
     }];
